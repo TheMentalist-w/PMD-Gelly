37b45755fc797c56055ad5338ca81c5c446f9bcc
TheMentalist-w <weronika.mro@interia.pl>
2022-05-21 14:19:09 +0200

    create graph from collection

--- src/GellyExample.scala
+++ src/GellyExample.scala
@@ -1,32 +1,42 @@
-import org.apache.flink.api.java.ExecutionEnvironment
+import java.util
+
+import org.apache.flink.api.java.{ExecutionEnvironment}
 import org.apache.flink.graph.{Edge, Graph, Vertex}
+import collection.JavaConversions._
+
+import scala.io.Source
 
 object GellyExample extends App {
         // 1) Tworzymy środowisko przetwarzania danych
         val env = ExecutionEnvironment.getExecutionEnvironment
 
-        // 2) Tworzymy węzły oraz krawędzie, z których zbudujemy graf
-        val vertexDS = env.fromElements(
-        new Vertex(0L, ("Alice", 42L)),
-        new Vertex(1L, ("Bob", 23L)),
-        new Vertex(2L, ("Eve", 84L))
-        )
-        val edgeDS = env.fromElements(
-        new Edge(0L, 1L, "23/01/1987"),
-        new Edge(1L, 2L, "12/12/2009")
-        )
+        // 2) Stacje key - id stacji, value - nazwa stacji
+        val vertexFile = Source.fromFile("./station.csv")
+        val vertices= vertexFile
+          .getLines()
+          .filter(!_.startsWith("\"station_id\""))
+          .map(_.split(","))
+          .map(array => new Vertex(array(0), array(1)))
+          .toArray
+          .toSeq
+        vertexFile.close()
+
+        // Krawedzie zawierają id roweru
+        val edgeFile = Source.fromFile("./trip.csv")
+        val edges = edgeFile
+          .getLines()
+          .filter(!_.startsWith("\"trip_id\""))
+          .map(_.split(","))
+          .filter(array => array(7).contains("-"))
+          .map(array => new Edge(array(7), array(8), array(3)))
+          .toArray
+          .toSeq
+        edgeFile.close()
 
         // 3) Tworzymy graf z węzłów i krawędzi
-        val graph = Graph.fromDataSet(vertexDS, edgeDS, env)
+        val graph = Graph.fromCollection(vertices, edges, env)
 
-        // 4) Dokonajmy w nim drobnej modyfikacji
-        val updatedGraph = graph.addEdges {
-        val edges : java.util.List[Edge[Long,String]] =
-        new java.util.ArrayList[Edge[Long,String]]()
-        edges.add(new Edge(0L, 2L, "27/04/2021"))
-        edges
-        }
         // 6) Uzyskajmy z grafu kogoś kto zna co najmniej dwie osoby
-        val someone = updatedGraph.outDegrees.filter(_.f1.getValue >= 2).collect().get(0)
+        val someone = graph.outDegrees.filter(_.f1.getValue >= 2).collect().get(0)
         println(someone.f0)
 }
