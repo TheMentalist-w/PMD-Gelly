20ecec3169a585e65d74f0bb066b455313a67e22
adam-handke <adam.handke@outlook.com>
2022-05-22 23:24:18 +0200

    fix printing ex aequo

--- src/GellyExample.scala
+++ src/GellyExample.scala
@@ -76,11 +76,11 @@ object GellyExample extends App {
   //                val someone = mostUsedGraph.outDegrees.filter(_.f1.getValue >= 2).collect().get(0)
   //                println(someone.f0)
 
-  val vertexMaxOutDeg = mostUsedGraph.outDegrees().maxBy(1).collect().get(0) // Najczęściej używana stacja jako startowa
-  val vertexMaxInDeg = mostUsedGraph.inDegrees().maxBy(1).collect().get(0) // Najczęściej używana stacja jako końcowa
+  val vertexMaxOutDeg = mostUsedGraph.outDegrees().maxBy(1).collect() // Najczęściej używana stacja jako startowa
+  val vertexMaxInDeg = mostUsedGraph.inDegrees().maxBy(1).collect() // Najczęściej używana stacja jako końcowa
 
   println("Zad.9")
-  println("Najczęściej używana stacja jako startowa: " + vertexMaxOutDeg.f0 + " (" + vertexMaxOutDeg.f1.toString + ")")
+  println("Najczęściej używana stacje jako startowe (nazwa, licznik): " + vertexMaxOutDeg)
 
-  println("Najczęściej używana stacja jako końcowa: " + vertexMaxInDeg.f0 + " (" + vertexMaxInDeg.f1.toString + ")")
+  println("Najczęściej używana stacje jako końcowe (nazwa, licznik): " + vertexMaxInDeg)
 }
